344eb9d288e9a29bd50f8eb3c74f0767c148ed5d
tu95ctv <nguyenductu@gmail.com>
2018-04-10 16:12:24 +0700

    2

--- dai_tgg/__manifest__.py
+++ dai_tgg/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','base_import','product','hr','stock'],
+    'depends': ['base','base_import','product','hr'],
 
     # always loaded
     'data': [
